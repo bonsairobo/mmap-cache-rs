decefde65cdafd37d7f01601a6edfe8861427c77
Duncan <bonsairobo@gmail.com>
2022-04-29 23:43:57 -0700

    impl std::error::Error for crate::Error

--- src/error.rs
+++ src/error.rs
@@ -6,6 +6,16 @@ pub enum Error {
     IO(io::Error),
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Fst(e) => e.fmt(f),
+            Self::IO(e) => e.fmt(f),
+        }
+    }
+}
+impl std::error::Error for Error {}
+
 impl From<fst::Error> for Error {
     fn from(e: fst::Error) -> Self {
         Self::Fst(e)
