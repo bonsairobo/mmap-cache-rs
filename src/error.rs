c23e2ff5131d16278fa1c6cbfd489289714e297e
Duncan <bonsairobo@gmail.com>
2022-06-27 13:55:17 -0700

    use thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,29 +1,11 @@
 use std::io;
 
-#[derive(Debug)]
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub enum Error {
-    Fst(fst::Error),
-    IO(io::Error),
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Fst(e) => e.fmt(f),
-            Self::IO(e) => e.fmt(f),
-        }
-    }
-}
-impl std::error::Error for Error {}
-
-impl From<fst::Error> for Error {
-    fn from(e: fst::Error) -> Self {
-        Self::Fst(e)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(e: io::Error) -> Self {
-        Self::IO(e)
-    }
+    #[error(transparent)]
+    Fst(#[from] fst::Error),
+    #[error(transparent)]
+    IO(#[from] io::Error),
 }
